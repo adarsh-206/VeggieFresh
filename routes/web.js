e300310721013ff08fd13723722c046cb29477f7
Adarsh Dhote <adarshdhote9926@gmail.com>
2022-05-22 22:04:16 +0530

    auth system

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const guest = require('../app/http/middlewares/guest')
 
 function initRoutes(app) {
 
@@ -10,11 +11,15 @@ app.get('/cart', cartController().index)
 
 app.post('/update-cart', cartController().update)
 
-app.get('/login', authController().login)
+app.get('/login', guest, authController().login)
 
-app.get('/register', authController().register)
+app.post('/login', authController().postLogin)
 
+app.get('/register', guest, authController().register)
+
+app.post('/register', authController().postRegister)
+
+app.post('/logout', authController().logout)
 }
-
 
 module.exports = initRoutes

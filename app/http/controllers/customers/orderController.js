6bf36ba0cc6b3355beb8bc3174321b773652b133
techie-adarsh <66101827+techie-adarsh@users.noreply.github.com>
2022-07-19 13:08:35 +0530

    Update orderController.js

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,6 +1,7 @@
 const Order = require('../../../models/order')
 const moment = require('moment')
-moment().format();
+var date = moment.utc().format();
+var local = moment.utc(date).local().format();
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 function orderController () {
     return {
